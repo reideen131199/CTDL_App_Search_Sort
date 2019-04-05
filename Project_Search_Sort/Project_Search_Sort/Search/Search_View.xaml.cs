0b971a5e7c330a686032a8b895224c9b24b5ce83
reideen131199 <reideen131199@gmail.com>
2019-04-06 01:44:08 +0700

    Add Event and Set Layout for Row List Algorithm Sort, Row Control of Layout Sort

---
+++ Project_Search_Sort/Project_Search_Sort/Search/Search_View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Collections.Generic;
+
+namespace Project_Search_Sort
+{
+    /// <summary>
+    /// Interaction logic for Search_View.xaml
+    /// </summary>
+    public partial class Search_View : UserControl
+    {
+        public Search_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Project_Search_Sort/Project_Search_Sort/Sort/Sort_View.xaml.cs
+++ Project_Search_Sort/Project_Search_Sort/Sort/Sort_View.xaml.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Collections.Generic;
 
 namespace Project_Search_Sort
 {
@@ -13,10 +12,18 @@ namespace Project_Search_Sort
     /// </summary>
     public partial class Sort_View : UserControl
     {
-        private Column_Control C;
+        #region Private Variable
+
+        private TextBlock chosseAlgorithm;
+        private List<TextBlock> AlgorithmSorts;
+        
+        #endregion
+
+        #region Constructor
         public Sort_View()
         {
             InitializeComponent();
+            CreateLayoutListSort();
             /*
             int[] a = { -1, 10, 64, 7, 52, 32, 18, 2, 48, 1, 99 };
             //int[] a = {-1, 1, 1, 5, 5, 8, 9, 2, 2, 3, 1 };
@@ -30,41 +37,103 @@ namespace Project_Search_Sort
             }
             */
         }
+        #endregion
 
-        private int[] CreateArr()
+        #region Row List Sort
+        /// <summary>
+        /// Create Layout List Sort
+        /// </summary>
+        private void CreateLayoutListSort()
         {
-            string[] arrInput = ("0," + ViewArray.Text).Split(',');
-            int[] res = new int[arrInput.Length];
-            try
-            {
-                for (int i = 1; i < arrInput.Length; i++)
-                {
-                    res[i] = Int16.Parse(arrInput[i]);
-                }
-            }
-            catch(Exception e)
-            {
-                ViewArray.Text = e.ToString();
-                return res;
-            }
+            string[] st = { "Bubble", "Selection", "Insert", "Counting",
+                            "Quick", "Shell", "Radix", "Merge", "Heap" };
 
-            return res;
+            AlgorithmSorts = new List<TextBlock>();
+            foreach (string s in st)
+            {
+                TextBlock textBlock = new TextBlock();
+                textBlock.Name = s;
+                textBlock.Text = s;
+                textBlock.FontSize = 16;
+                textBlock.TextAlignment = TextAlignment.Center;
+                textBlock.Margin = new Thickness(0, 3, 0, 3);
+                textBlock.Padding = new Thickness(5, 3, 5, 3);
+                textBlock.MouseDown += TextBlock_SelecAlgSort;
+                AlgorithmSorts.Add(textBlock);
+                LayoutListAlgorithmSort.Children.Add(textBlock);
+            }
+            chosseAlgorithm = AlgorithmSorts[0];
+            chosseAlgorithm.FontWeight = FontWeights.Bold;
         }
 
-        private void TextBlock_MouseUp_ChosserAlg(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        /// <summary>
+        /// Event Select Algorithm Sort
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TextBlock_SelecAlgSort(object sender, RoutedEventArgs e)
         {
-
+            chosseAlgorithm.FontWeight = FontWeights.Normal;
+            chosseAlgorithm = (TextBlock)sender;
+            chosseAlgorithm.FontWeight = FontWeights.Bold;
         }
+        #endregion
+
+        #region Row Control
 
         private void ViewArray_LostFocus(object sender, RoutedEventArgs e)
         {
-            ViewArray.Text = string.Join(", ", CreateArr());
+            // Send ViewArray.Text to Layout Animation Algorithm
         }
 
-        private void Button_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        private void Button_RandomArr(object sender, RoutedEventArgs e)
         {
             Random rand = new Random();
+            int n = rand.Next(20) + 1;
+
+            int[] arr = new int[n + 1];
+            for (int i = 1; i <= n; i++)
+            {
+                arr[i] = rand.Next(50) + 1;
+            }
+            arr[0] = n;
+            ViewArray.Text = string.Join(", ", arr);
+            // Send ViewArray.Text to Layout Animation Algorithm
+        }
+
+        private void Button_Pause(object sender, RoutedEventArgs e)
+        {
+            /*
+            if (true)
+            {
+                Btn_Pause.Content = "Resume";
+            }
+            else
+            {
+                Btn_Pause.Content = "Pause";
+            }
+            */
+        }
+
+        private void Button_End(object sender, RoutedEventArgs e)
+        {
 
         }
+
+        private void Button_StartSort(object sender, RoutedEventArgs e)
+        {
+            Sorting();
+            // Run Animation Layout Sort
+        }
+
+        private void Sorting()
+        {
+            Btn_Pause.Content = "Resume";
+            Btn_End.IsEnabled = true;
+            Btn_StartSort.IsEnabled = false;
+        }
+
+        #endregion
+
     }
 }
