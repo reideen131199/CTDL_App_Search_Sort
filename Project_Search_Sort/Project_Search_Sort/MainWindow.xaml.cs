44c3f76d3dad3b3c2f94579f61af7d02ebdb0a45
reideen131199 <reideen131199@gmail.com>
2019-04-05 20:21:26 +0700

    Add animation between Show and Hide of Layout, Introduction in MainWindow

--- Project_Search_Sort/Project_Search_Sort/MainWindow.xaml.cs
+++ Project_Search_Sort/Project_Search_Sort/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Windows;
-using System.Windows.Media;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
@@ -17,39 +16,6 @@ namespace Project_Search_Sort
         public MainWindow()
         {
             InitializeComponent();
-
-            /*
-            int[] a = { -1, 10, 64, 7, 52, 32, 18, 2, 48, 1, 99 };
-            //int[] a = {-1, 1, 1, 5, 5, 8, 9, 2, 2, 3, 1 };
-            Sort s = new Sort(10, a);
-            s.CountingSort();
-            a = s.getArray();
-            for (int i = 1; i <= 10; i++)
-            {
-                //sx.Text += a[i];
-                //sx.Text += " ";
-            }
-            */
-        }
-
-        private void goLayout()
-        {
-            DoubleAnimation show = new DoubleAnimation(1, new Duration(TimeSpan.FromSeconds(0.75)));
-            DoubleAnimation hide = new DoubleAnimation(0, new Duration(TimeSpan.FromSeconds(0.75)));
-            DoubleAnimation resize = new DoubleAnimation(30, new Duration(TimeSpan.FromSeconds(0.5)));
-
-            BackIntro.Visibility = Visibility.Visible;
-            BackIntro.Opacity = 0;
-
-            BackIntro.BeginAnimation(OpacityProperty, show);
-            Layout.BeginAnimation(OpacityProperty, show);
-            Intro.BeginAnimation(OpacityProperty, hide);
-            Eagle_Team.BeginAnimation(FontSizeProperty, resize);
-
-            Task.Run(async () => {
-                await Task.Delay(1000);
-                Intro.Visibility = Visibility.Hidden;
-            });
         }
 
         private void goIntro()
@@ -57,8 +23,8 @@ namespace Project_Search_Sort
             DoubleAnimation show = new DoubleAnimation(1, new Duration(TimeSpan.FromSeconds(0.75)));
             DoubleAnimation hide = new DoubleAnimation(0, new Duration(TimeSpan.FromSeconds(0.75)));
             DoubleAnimation resize = new DoubleAnimation(40, new Duration(TimeSpan.FromSeconds(0.5)));
-
             Intro.Visibility = Visibility.Visible;
+
             Intro.BeginAnimation(OpacityProperty, show);
             Layout.BeginAnimation(OpacityProperty, hide);
             BackIntro.BeginAnimation(OpacityProperty, hide);
@@ -73,6 +39,7 @@ namespace Project_Search_Sort
         }
 
         #region Event Button on Intro
+
         private void Button_Sort(object sender, RoutedEventArgs e)
         {
             Child_Layout = new Sort_View();
@@ -93,6 +60,26 @@ namespace Project_Search_Sort
             this.Close();
         }
         #endregion
+
+        private void goLayout()
+        {
+            DoubleAnimation show = new DoubleAnimation(1, new Duration(TimeSpan.FromSeconds(0.75)));
+            DoubleAnimation hide = new DoubleAnimation(0, new Duration(TimeSpan.FromSeconds(0.75)));
+            DoubleAnimation resize = new DoubleAnimation(30, new Duration(TimeSpan.FromSeconds(0.5)));
+
+            BackIntro.Visibility = Visibility.Visible;
+            BackIntro.Opacity = 0;
+            BackIntro.BeginAnimation(OpacityProperty, show);
+
+            Layout.BeginAnimation(OpacityProperty, show);
+            Intro.BeginAnimation(OpacityProperty, hide);
+            Eagle_Team.BeginAnimation(FontSizeProperty, resize);
+
+            Task.Run(async () => {
+                await Task.Delay(1000);
+                Intro.Visibility = Visibility.Collapsed;
+            });
+        }
 
         #region Event Button on Layout
 
--- Project_Search_Sort/Project_Search_Sort/Sort/Sort_View.xaml.cs
+++ Project_Search_Sort/Project_Search_Sort/Sort/Sort_View.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,14 +17,53 @@ namespace Project_Search_Sort
         public Sort_View()
         {
             InitializeComponent();
-            C = new Column_Control();
-            LayoutAnimation.Children.Add(C);
+            /*
+            int[] a = { -1, 10, 64, 7, 52, 32, 18, 2, 48, 1, 99 };
+            //int[] a = {-1, 1, 1, 5, 5, 8, 9, 2, 2, 3, 1 };
+            Sort s = new Sort(10, a);
+            s.CountingSort();
+            a = s.getArray();
+            for (int i = 1; i <= 10; i++)
+            {
+                //sx.Text += a[i];
+                //sx.Text += " ";
+            }
+            */
+        }
 
-            Task.Run(async () => { await Task.Delay(1000); C.col.Val = 30; });
+        private int[] CreateArr()
+        {
+            string[] arrInput = ("0," + ViewArray.Text).Split(',');
+            int[] res = new int[arrInput.Length];
+            try
+            {
+                for (int i = 1; i < arrInput.Length; i++)
+                {
+                    res[i] = Int16.Parse(arrInput[i]);
+                }
+            }
+            catch(Exception e)
+            {
+                ViewArray.Text = e.ToString();
+                return res;
+            }
+
+            return res;
         }
 
         private void TextBlock_MouseUp_ChosserAlg(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
+
+        }
+
+        private void ViewArray_LostFocus(object sender, RoutedEventArgs e)
+        {
+            ViewArray.Text = string.Join(", ", CreateArr());
+        }
+
+        private void Button_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Random rand = new Random();
 
         }
     }
